3fda00d111d102d1a10a0e9e8d0c7221379220b8
thejonroberts <jon.roberts.8@gmail.com>
2017-06-09 00:39:56 -0500

    create javascript plan and layout

---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+//blog entries
+const blogEntry = {
+	date: "June 1, 2017",
+	title: "Week 1",
+	paragraphs: {"lorem ipsum", 
+				"borem impsome"
+				}
+	// imageURL: ,
+	};
+
+//put blog entry into blog posts
+//let blogPosts = {};
+//function combineEntries() {}
+
+//format posts to HTML
+let blog = document.getElementById("blogPosts");
+
+function formatPostHTML() {}
+
+//put all blog posts into Blog Section
+let blogSection = {};
+function formaBlogSection() {}
+
+
+// <section id="blog">
+// 			<h2>Blog</h2>
+// 			<div class="sectionFlexContainer">
+// 				<!-- PRIMARY-CONTENT -->
+// 				<div class="primaryContent">
+// 					<article>
+// 							<h3>Heading</h3>
+// 							<p></p>
+// 					</article>
+// 				</div> <!-- primaryContent -->
+// 				<!-- SECONDARY CONTENT -->
+// 				<div class="secondaryContent">
+// 					<article>
+// 						<h5>Extra Info</h5>
+// 						<p></p>
+// 						<p></p>
+// 					</article>
+// 				</div><!-- secondaryContent -->
+// 				<!-- TERTIARY CONTENT -->
+// 				<div class="tertiaryContent">
+// 					<article>
+// 					<img src="./images/jonRoberts200.png" alt="Photo of Jon Roberts">
+// 					<h5>Extra Info</h5>
+// 						<p></p>
+// 					</article>
+// 				</div><!-- tertiaryContent -->
+// 			</div><!-- sectionFlexContainer -->	
+// 		</section><!-- SECTION ID -->
