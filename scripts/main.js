a1ef312187c87b2f909ec06d16684ee76f956e7d
thejonroberts <jon.roberts.8@gmail.com>
2017-06-09 02:08:24 -0500

    write blog example post to DOM using for loops

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,56 @@
 //blog entries
-const blogEntry = {
-	date: "June 1, 2017",
-	title: "Week 1",
-	paragraphs: ["Monday: lorem ipsum", 
+const blogEntries = [
+	{
+		date: "June 1, 2017",
+		title: "Week 1",
+		paragraphs: ["Monday: lorem ipsum", 
+					"Tuesday: snorem skipsum",
+					"Wednesday: moer ipsum",
+					"Thursday: some stuff",
+					"Friday: Lab times"
+					]
+		// imageURL: ,
+	},
+	{
+		date: "June 8, 2017",
+		title: "Week 2",
+		paragraphs: ["Monday: lorem ipsum", 
 				"Tuesday: snorem skipsum",
-				"Wednesday: moer ipsum"
+				"Wednesday: moer ipsum",
+				"Thursday: some stuff",
+				"Friday: Lab times"
 				]
-	// imageURL: ,
-	};
-	// console.log('blogEntry', blogEntry);
+		// imageURL: ,
+	}];
+	// console.log('blogEntry',blogEntries.length, blogEntries);
 
-//put blog entry into blog posts
-//let blogPosts = {};
-//function combineEntries() {}
-
-//format posts to HTML
+//initialize Blog DOM element
 let blog = document.getElementById("blogPosts");
 let blogPostHTML = "";
 
-function formatPostHTML() {
+//format individualposts to HTML
+function formatPostHTML(j) {
 	let paragraphHTMLString = "";
-	for (i=0; i < blogEntry.paragraphs.length; i++) {
-		paragraphHTMLString += `<p>${blogEntry.paragraphs[i]}</p>`;
-		};
-	blogPostHTML = `<article>
-					<h3>${blogEntry.title}</h3>
-					${paragraphHTMLString}
-					<span>${blogEntry.date}</span>
-				</article>`
-}
-formatPostHTML();
-console.log('blogPost', blogPostHTML);
+	for (i=0; i < blogEntries[j].paragraphs.length; i++) {
+	paragraphHTMLString += `<p>${blogEntries[j].paragraphs[i]}</p>`;
+	}
+	blogPostHTML += `<article>
+				<h3>${blogEntries[j].title}</h3>
+				<span>${blogEntries[j].date}</span>
+				${paragraphHTMLString}
+				</article>`;
+	}
 
-//write blogPost to DOM
+//create total blog looping through individual HTML
+function combinePosts() {
+	for (var j = (blogEntries.length - 1); j >= 0; j--) {
+		formatPostHTML(j);
+	}
+}
+combinePosts();		// console.log('blogPost', blogPostHTML);
+
+//write combined posts to DOM
 function blogPostDOM() {
-	blog.innerHTML += blogPostHTML;
+	blog.innerHTML = blogPostHTML;
 }
 blogPostDOM();
-
-//put all blog posts into Blog Section
-// let blogSection = {};
-// function formaBlogSection() {}
-
-
-// <section id="blog">
-// 			<h2>Blog</h2>
-// 			<div class="sectionFlexContainer">
-// 				<!-- PRIMARY-CONTENT -->
-// 				<div class="primaryContent">
-// 					<article>
-// 							<h3>Heading</h3>
-// 							<p></p>
-// 					</article>
-// 				</div> <!-- primaryContent -->
-// 				<!-- SECONDARY CONTENT -->
-// 				<div class="secondaryContent">
-// 					<article>
-// 						<h5>Extra Info</h5>
-// 						<p></p>
-// 						<p></p>
-// 					</article>
-// 				</div><!-- secondaryContent -->
-// 				<!-- TERTIARY CONTENT -->
-// 				<div class="tertiaryContent">
-// 					<article>
-// 					<img src="./images/jonRoberts200.png" alt="Photo of Jon Roberts">
-// 					<h5>Extra Info</h5>
-// 						<p></p>
-// 					</article>
-// 				</div><!-- tertiaryContent -->
-// 			</div><!-- sectionFlexContainer -->	
-// 		</section><!-- SECTION ID -->
